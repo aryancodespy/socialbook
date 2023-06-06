4d8c6ffc1e2d038a8b2ff0e1e475af30d37952f3
Aryan <125204827+aryancodespy@users.noreply.github.com>
2023-06-06 16:41:00 +0600

    Add dark mode function

--- script/script.js
+++ script/script.js
@@ -3,3 +3,10 @@ const dropdownMenu = document.querySelector(".dropdown-menu");
 function settingsMenu(){
     dropdownMenu.classList.toggle("dropdown-menu-height");
 }
+
+const darkMode = document.querySelector("#dark-btn");
+
+darkMode.onclick = function(){
+    darkMode.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme")
+}
