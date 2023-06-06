bdc4376169984132d7a28765d74486fc8ad6ce0e
Aryan <125204827+aryancodespy@users.noreply.github.com>
2023-06-06 16:49:16 +0600

    Modify local storage

--- script/script.js
+++ script/script.js
@@ -8,5 +8,25 @@ const darkMode = document.querySelector("#dark-btn");
 
 darkMode.onclick = function(){
     darkMode.classList.toggle("dark-btn-on");
-    document.body.classList.toggle("dark-theme")
+    document.body.classList.toggle("dark-theme");
+
+    if (localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+if (localStorage.getItem("theme") == "light"){
+    darkMode.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if (localStorage.getItem("theme") == "dark"){
+    darkMode.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+
+else {
+    localStorage.setItem("theme", "light");
 }
